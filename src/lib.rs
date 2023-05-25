a76453faf162e0b1bb88b59d355ff07f2fdac0bf
Dave Kozma <dkozma@adobe.com>
2023-05-25 17:28:37 -0400

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ fn add_to_resource_object(
     obj: &Handle<JsObject>,
     key: &str,
     value: &[u8],
-) -> NeonResult<()> {
+) -> Result<()> {
     let data = JsUint8Array::from_slice(cx, value)?;
     obj.set(cx, key.as_ref(), data)?;
     Ok(())
@@ -19,9 +19,8 @@ fn read_asset(mut cx: FunctionContext) -> JsResult<JsObject> {
     let format = cx.argument::<JsString>(0)?.value(&mut cx);
     let buffer = cx.argument::<JsBuffer>(1)?;
     let data = buffer.as_slice(&cx);
-    // FIXME - no unwraps
-    let store = ManifestStore::from_bytes(&format, data, true).unwrap();
-    let serialized_store = cx.string(serde_json::to_string(&store).unwrap());
+    let store = ManifestStore::from_bytes(&format, data, true)?;
+    let serialized_store = serde_json::to_string(&store).map(|s| cx.string(s))?;
     let response_obj = cx.empty_object();
     let resource_store = cx.empty_object();
 
@@ -33,8 +32,8 @@ fn read_asset(mut cx: FunctionContext) -> JsResult<JsObject> {
             let manifest_resources = manifest.resources().resources();
             let manifest_resource_obj = cx.empty_object();
             // Process the manifest's ResourceStore
-            manifest_resources.iter().for_each(|(k, v)| {
-                add_to_resource_object(&mut cx, &manifest_resource_obj, k, v).unwrap();
+            manifest_resources.iter().try_for_each(|(k, v)| {
+                add_to_resource_object(&mut cx, &manifest_resource_obj, k, v)
             });
             // Resources for the manifest are stored in a separate store from the ingredients, so we need to
             // separately process the ingredients' ResourceStores
@@ -44,13 +43,10 @@ fn read_asset(mut cx: FunctionContext) -> JsResult<JsObject> {
                     .resources()
                     .iter()
                     .for_each(|(k, v)| {
-                        add_to_resource_object(&mut cx, &manifest_resource_obj, k, v).unwrap();
+                        add_to_resource_object(&mut cx, &manifest_resource_obj, k, v)
                     });
             });
-            // FIXME - no unwraps
-            resource_store
-                .set(&mut cx, label.as_ref(), manifest_resource_obj)
-                .unwrap();
+            resource_store.set(&mut cx, label.as_ref(), manifest_resource_obj)?;
         });
 
     response_obj.set(&mut cx, "serializedStore", serialized_store)?;
@@ -59,13 +55,8 @@ fn read_asset(mut cx: FunctionContext) -> JsResult<JsObject> {
     Ok(response_obj)
 }
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello node!"))
-}
-
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("hello", hello)?;
     cx.export_function("read_asset", read_asset)?;
     Ok(())
 }
